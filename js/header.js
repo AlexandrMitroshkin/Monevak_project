3b9e06a7ff6a70898f3d7bd4191cc4a221e316bf
Alexander.Mitroshkin <alexander.a.mitroshkin@gmail.com>
2025-06-01 19:48:07 +0300

    comit15_before_depseeks_edits

---
+++ js/header.js
@@ -0,0 +1,22 @@
+                                            // реализация moreInfButton
+let MoreInfButton = document.querySelector(".header-article1-moreInfButton")
+let headerArticle1list = document.querySelector(".header-article1-list")
+
+.MoreInfButton.onclick{
+
+}
+
+
+
+
+// let headerNavMore = document.querySelector(".header-nav-more");
+// let headerNavLinks = document.querySelector(".header-nav-links");
+
+// headerNavMore.onclick = function(){
+//   headerNavSearch.classList.remove("visible");
+//   headerNavLinks.classList.toggle("visible");
+//   headerNavMore.classList.toggle("inverted");
+//   if (headerNavLinks.classList.contains("visible")){
+//     header.style.marginBottom = "60px";
+//   }
+// }
